2960285951609814dde5df8b142066eb1e6ff185
surya-ngpws <suryad@ngpwebsmart.com>
2023-10-12 07:57:36 +0530

    carousel fixed

--- src/screens/DashboardScreens/SwipeCarousel.tsx
+++ src/screens/DashboardScreens/SwipeCarousel.tsx
@@ -17,47 +17,38 @@ import Animated, {
   Easing,
   Keyframe,
   FadeIn,
-  FadeOut
+  FadeOut,
 } from "react-native-reanimated";
-
+import Carousel from "react-native-reanimated-carousel";
+import PaginationItem from "./Pagination";
+import { scrollTo } from "react-native-reanimated";
 const data = [
   { chartColor: COLORS.chartLinePink, bgColor: "#b91e7a66" },
   { chartColor: COLORS.chartLineBlue, bgColor: "#0C66B166" },
   { chartColor: COLORS.chartLinePurple, bgColor: "#9368FB66" },
-  { chartColor: COLORS.chartLineRed, bgColor: "#F44C4566" }
+  { chartColor: COLORS.chartLineRed, bgColor: "#F44C4566" },
 ];
 // const DeviceWidth = Dimensions.get('window').width;
 const AnimatedSwiper = Animated.createAnimatedComponent(View);
-
+const colors = ["#899F9C", "#899F9C", "#899F9C", "#899F9C"];
 const SwipeCarousel = () => {
-  const swiperRef = useRef(null);
-  const opacity = useSharedValue(0);
-  const progressValue = useSharedValue(0.8);
-  const handleScroll = () => {
-    progressValue.value = withSequence(
-      withTiming(0),
-      withTiming(0.8, { duration: 3000 })
-    );
-    opacity.value = withSequence(
-      withTiming(0),
-      withTiming(1, {
-        duration: 2000,
-        easing: Easing.out(Easing.cubic)
-      })
-    );
+  const swiper = useRef(null);
+  const width = Dimensions.get("window").width;
+  const [pagingEnabled, setPagingEnabled] = React.useState<boolean>(true);
+  const [snapEnabled, setSnapEnabled] = React.useState<boolean>(true);
+  const progressValue = useSharedValue<number>(0);
+  const onClicking = (index) => {
+    (swiper.current as any).scrollTo(index);
   };
-
-  const handleEnd = () => {
-    opacity.value = withTiming(1);
-  };
-  console.log(progressValue.value);
-
   return (
     <View style={{ flex: 1 }}>
-      <Swiper
+      {/* <Swiper
+      ref={swiperRef}
         springConfig={{ bounciness: 0 }}
-        gesturesEnabled={CarouselGesture}
+        // gesturesEnabled={CarouselGesture}
+       
         from={0}
+        minDistanceToCapture={2}
         minDistanceForAction={0.1}
         controlsProps={{
           prevPos: false,
@@ -68,9 +59,7 @@ const SwipeCarousel = () => {
         }}
       >
         {data.map((item, index) => (
-          <Animated.View
-            onPointerDown={handleScroll}
-            style={{ opacity: opacity }}
+          <View
             key={index}
           >
             <Box flexDirection="row" ml={20} mt={20}>
@@ -96,9 +85,77 @@ const SwipeCarousel = () => {
               </Box>
             </Box>
             <InteractiveChart chartColor={item.chartColor} />
-          </Animated.View>
+          </View>
         ))}
-      </Swiper>
+      </Swiper> */}
+
+      <Carousel
+        ref={swiper}
+        loop={false}
+        width={width - 18}
+        // height={width / 2}
+        // autoPlay={true}
+        pagingEnabled={true}
+        snapEnabled={true}
+        onProgressChange={(_, absoluteProgress) =>
+          (progressValue.value = absoluteProgress)
+        }
+        data={data}
+        scrollAnimationDuration={100}
+        onSnapToItem={(index) => console.log("current index:", index)}
+        renderItem={({ item, index }) => (
+          <View key={index}>
+            <Box flexDirection='row' ml={20} mt={20}>
+              <Box position='relative'>
+                <ProgressCircle
+                  style={{ height: 100, width: 100 }}
+                  progress={progressValue.value}
+                  progressColor={item.chartColor}
+                  backgroundColor={item.bgColor}
+                />
+                <Box position='absolute' left={32} top={30}>
+                  <Text fontWeight='$bold'>24&deg;C</Text>
+                  <Text fontSize='$sm' color='#B7B7B7'>
+                    Temp
+                  </Text>
+                </Box>
+              </Box>
+              <Box ml={30} mt={20}>
+                <Text>30% Lower Today</Text>
+                <Text fontSize='$sm' color='#B7B7B7'>
+                  Updated 10mins ago
+                </Text>
+              </Box>
+            </Box>
+            <InteractiveChart chartColor={item.chartColor} />
+          </View>
+        )}
+      />
+      {!!progressValue && (
+        <View
+          style={{
+            marginTop: -40,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: 70,
+            alignSelf: "center",
+          }}
+        >
+          {colors.map((backgroundColor, index) => {
+            return (
+              <PaginationItem
+                handlePage={swiper}
+                backgroundColor={backgroundColor}
+                animValue={progressValue}
+                index={index}
+                key={index}
+                // isRotate={isVertical}
+                length={colors.length}
+              />
+            );
+          })}
+        </View>
+      )}
     </View>
   );
 };
@@ -107,19 +164,19 @@ export default SwipeCarousel;
 
 const fadeIn = new Keyframe({
   0: {
-    opacity: 0
+    opacity: 0,
   },
   100: {
-    opacity: 1
-  }
+    opacity: 1,
+  },
 });
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   heading: {
-    fontSize: 44
+    fontSize: 44,
   },
   wrapper: {},
   slide: {},
@@ -129,10 +186,10 @@ const styles = StyleSheet.create({
   text: {
     color: "#fff",
     fontSize: 30,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   img: {
     width: 310,
-    height: 350
-  }
+    height: 350,
+  },
 });
